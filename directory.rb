0a7eb2151ac497045fce7c143343a7228be60416
masha-b <mashabudryte@gmail.com>
2019-02-18 10:36:24 +0000

    methods for header, list and footer added

--- directory.rb
+++ directory.rb
@@ -12,11 +12,19 @@ students = [
   "Norman Bates"
 ]
 
-puts "The students of Villains Academy"
-puts "-------------"
-
-students.each do |student|
-  puts student
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
 end
 
-puts "Overall, we have #{students.count} great students"
+print_header
+print(students)
+print_footer(students)
